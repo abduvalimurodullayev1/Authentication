fc79d5f83b1bf5f267c63fc4e4f36d89fc07198b
abduvalimurodullayev <abduvalimurodullayev9@gmail.com>
2025-06-02 09:35:37 +0500

    Register Serializer fixed

--- apps/users/api_endpoints/RegisterApiView/serializer.py
+++ apps/users/api_endpoints/RegisterApiView/serializer.py
@@ -1,26 +1,22 @@
 from rest_framework import serializers
 from django.utils import timezone
 from apps.users.models import User
+import uuid
 from apps.users.helpers import *
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8, required=True)
     email = serializers.EmailField(required=True)
-    username = serializers.CharField(required=True)
+    full_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'full_name', 'password']
 
     def validate_email(self, value):
         if User.objects.filter(email__iexact=value).exists():
             raise serializers.ValidationError("Email already in use.")
-        return value
-
-    def validate_username(self, value):
-        if User.objects.filter(username__iexact=value).exists():
-            raise serializers.ValidationError("Username already taken.")
         return value
 
     def create(self, validated_data):
@@ -28,8 +24,9 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         user = User(
             email=validated_data['email'].lower(),
-            username=validated_data['username'],
-            full_name=validated_data.get('username')
+            full_name=validated_data.get('full_name'),
+            username=str(uuid.uuid4())[:30]
+
         )
         user.set_password(validated_data['password'])
         user.date_joined = timezone.now()
